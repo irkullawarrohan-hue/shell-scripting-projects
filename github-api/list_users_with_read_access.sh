c97be240746779f9e8aa8159b91986f53d86222e
irkullawarrohan <irkullawarrohan@gmail.com>
2026-04-22 10:48:24 +0530

    Implement argument validation in list_users_with_read_access.sh
    Added a helper function to validate command line arguments.

--- github-api/list_users_with_read_access.sh
+++ github-api/list_users_with_read_access.sh
@@ -5,6 +5,8 @@
 # Purpose: List users with READ access
 # -------------------------------
 
+helper()
+
 API_URL="https://api.github.com"
 
 USERNAME=$username
@@ -33,3 +35,10 @@ list_users_with_read() {
         echo "$users"
     fi
 }
+
+helper() {
+  expected_cmd_args=2
+  if [ $# -ne $expected_cmd_args ]; then
+  echo "Pass the required arguments/parameters"
+  fi
+  }
